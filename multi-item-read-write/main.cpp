edbe534e767f311493eb8669455034d131a2b956
Wang Guibao <wang_guibao@163.com>
2023-06-06 11:13:39 +0800

    Create main.cpp

---
+++ multi-item-read-write/main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <thread>
+#include "circularbuffer.h"
+
+CircularBuffer<int> buffer(DEFAULT_CIRCULAR_BUFFER_SIZE);
+
+bool running = true;
+
+void writeThread() {
+    int meter = 0;
+    int buf[10];
+
+    while (running) {
+        for (size_t i = 0; i < 10; ++i) {
+            buf[i] = meter++;
+        }
+
+        buffer.write(buf, 10);
+    }
+}
+
+void readThread() {
+    while (running) {
+        int buf[10];
+        buffer.read(buf, 10);
+#if 1
+        for (size_t i = 0; i < 10; ++i) {
+            printf("%d\n", buf[i]);
+        }
+#endif
+    }
+}
+
+int main()
+{
+    std::thread t1 = std::thread(writeThread);
+    std::thread t2 = std::thread(readThread);
+    std::this_thread::sleep_for(std::chrono::seconds(2));
+
+    running = false;
+
+    buffer.stop();
+
+    t1.join();
+    t2.join();
+
+    return 0;
+}
